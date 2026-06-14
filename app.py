dbb20c46b443c6b12169b0401cf6cf329844e037
ByteForge <premsharma.0308@gmail.com>
2026-06-14 21:52:53 +0545

    Add files via upload

--- app.py
+++ app.py
@@ -43,7 +43,8 @@ def scan():
 
     return jsonify({"result": "No QR found"})
 
+import os
+
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
