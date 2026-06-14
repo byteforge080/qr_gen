72be5934cc4490318f09ca0f0957b943bbe6a352
ByteForge <premsharma.0308@gmail.com>
2026-06-14 21:38:38 +0545

    Add files via upload

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request, jsonify
+import qrcode
+import os
+from pyzbar.pyzbar import decode
+from PIL import Image
+
+app = Flask(__name__)
+
+GENERATED_FOLDER = "static/generated"
+if not os.path.exists(GENERATED_FOLDER):
+    os.makedirs(GENERATED_FOLDER)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+# ---------------- QR GENERATE ----------------
+@app.route("/generate", methods=["POST"])
+def generate():
+    data = request.json.get("data")
+
+    if not data:
+        return jsonify({"error": "No data"}), 400
+
+    filepath = os.path.join(GENERATED_FOLDER, "qr.png")
+
+    img = qrcode.make(data)
+    img.save(filepath)
+
+    return jsonify({"image": "/static/generated/qr.png"})
+
+# ---------------- QR SCAN FROM FILE ----------------
+@app.route("/scan", methods=["POST"])
+def scan():
+    file = request.files["file"]
+
+    img = Image.open(file)
+    decoded = decode(img)
+
+    if decoded:
+        result = decoded[0].data.decode("utf-8")
+        return jsonify({"result": result})
+
+    return jsonify({"result": "No QR found"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
